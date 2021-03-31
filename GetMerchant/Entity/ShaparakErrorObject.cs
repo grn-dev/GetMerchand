868586e1525fc59c2aa772f4860346e283b995fa
mahmoud <Mahmoudsabzalii@Gmail.com>
2021-04-01 00:44:19 +0430

    add exception

--- GetMerchant/Entity/ShaparakErrorObject.cs
+++ GetMerchant/Entity/ShaparakErrorObject.cs
@@ -1,16 +1,21 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GetMerchant.Entity
 {
     public class ShaparakErrorObject
-    { 
+    {
+        [Required]
+        [Description("کد خطا")]
         [JsonProperty("codeExceptionValue")]
         public int CodeExceptionValue { get; set; }
 
-        
- 
+        public ShaparakCodeExceptions CodeException
+            => (ShaparakCodeExceptions)CodeExceptionValue;
 
-     
+
+        [Description("فیلد دارای خطا"), MaxLength(50),MinLength(2)]
         public string FieldName { get; set; }
     }
 }
